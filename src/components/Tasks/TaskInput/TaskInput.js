bfa320cb53b44f91e45f26f46948560cb2559173
Leonid Kulik <lkwebdev@mail.ru>
2023-09-10 17:36:48 +0300

    RTL#2: инлайн стили

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -4,22 +4,32 @@ import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
 
 const TaskInput = (props) => {
-  const [inputText, setinputText] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [isInputValid, setIsInputValid] = useState(true);
 
   const taskInputChangeHandler = (event) => {
-    setinputText(event.target.value);
+    if (event.target.value.trim()  > 0) {
+      setIsInputValid(true)
+    }
+
+    setInputText(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (inputText.trim().length === 0) {
+      setIsInputValid(false);
+      return;
+    }
+
     props.onAddTask(inputText);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
+        <label style={{color: !isInputValid ? 'red' : 'black'}}>Задачи</label>
+        <input style={{border: `1px solid ${isInputValid ? 'green' : 'red'}`}} type="text" onChange={taskInputChangeHandler} />
       </div>
       <Button type="submit">Добавить Задачу</Button>
     </form>
